18a42f030195ec7054dfc3e779bf8c5e313ce2a2
Avelino <31996+avelino@users.noreply.github.com>
2024-01-09 10:51:05 -0300

    auto lint markdown
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/main/tldr.cljs
+++ src/main/tldr.cljs
@@ -1,23 +1,27 @@
 (ns tldr
-  (:require [lambdaisland.fetch :as fetch]
+  (:require [clojure.string :as string]
+            [lambdaisland.fetch :as fetch]
             [promesa.core :as p]
             [util :refer [devlog]]))
 
 (def ^:private api-host
-  (if goog.DEBUG
-    "http://127.0.0.1:8000/"
-    "https://api.tldr.chat/"))
+  "https://api.tldr.chat/")
+
+(defn format-md-output
+  "auto fix markdown output from tldr.chat"
+  [content]
+  ;; TODO: use a library that auto-fixed markdown
+  (string/replace content "- " "* "))
 
 ;; curl -H 'Accept: text/markdown' https://tldr.chat/url/https://xkcd.com/1438/
 (defn- get-url
   "GET url from tldr.chat in format of markdown"
   [url]
   (p/let [req-url (str api-host "v0/url/" url)
-          resp (fetch/get req-url {:headers {:Accept "text/markdown" } })
+          resp (fetch/get req-url {:headers {:Accept "text/markdown"}})
           body (:body resp)]
-    (js/console.log "response: " resp)
-    (devlog "tldr/get-url" req-url body)
-    resp))
+    (devlog "tldr/get-url" req-url (format-md-output body))
+    (assoc resp :body (format-md-output body))))
 
 ;; curl -X POST --data url=https://xkcd.com/1438/ https://tldr.chat/summarize
 (defn summarize-url
@@ -25,7 +29,7 @@
   [url]
   (p/let [resp (fetch/post (str api-host "v0/summarize")
                            {:headers {:Content-Type "application/json"}
-                            :body (fetch/encode-body :json {:url url} )})
+                            :body (fetch/encode-body :json {:url url})})
           retry-countdown (atom 10)]
     (while (and (not= (:status (get-url url)) 200)
                 (pos? @retry-countdown))
@@ -33,3 +37,4 @@
       (p/delay 100000 "sleep 10s")
       (swap! retry-countdown dec))
     (get-url url)))
+    
