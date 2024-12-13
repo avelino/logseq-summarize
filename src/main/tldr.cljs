357d51e8af4a14493a10af2caeca17c69ecc063f
Avelino <31996+avelino@users.noreply.github.com>
2024-12-13 07:10:25 -0300

    implement infinite loop in request to handle status code in return for the loop
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/main/tldr.cljs
+++ src/main/tldr.cljs
@@ -5,7 +5,7 @@
             [util :refer [devlog]]))
 
 (def ^:private api-host
-  "https://api.tldr.chat/")
+  "https://api.tldr.chat/v0")
 
 (defn format-md-output
   "auto fix markdown output from tldr.chat"
@@ -17,7 +17,7 @@
 (defn- get-url
   "GET url from tldr.chat in format of markdown"
   [url]
-  (p/let [req-url (str api-host "v0/url/" url)
+  (p/let [req-url (str api-host "/url/" url)
           resp (fetch/get req-url {:headers {:Accept "text/markdown"}})
           body (:body resp)]
     (devlog "tldr/get-url" req-url (format-md-output body))
@@ -27,16 +27,25 @@
 (defn summarize-url
   "POST url to tldr.chat and get summary in format of markdown"
   [url]
-  (p/let [resp (fetch/post (str api-host "v0/summarize")
-                           {:headers {:Content-Type "application/json"}
-                            :body (fetch/encode-body :json {:url url})})
-          retry-countdown (atom 20)
+  (p/let [_ (fetch/post (str api-host "/summarize")
+                        {:headers {:Content-Type "application/json"}
+                         :body (fetch/encode-body :json {:url url})}) ;; first request to notify tldr.chat that we are waiting
           request-tldr (get-url url)]
-    (while (and (not= (:status request-tldr) 200)
-                (pos? @retry-countdown))
-      (devlog "tldr/summarize-url" url @retry-countdown)
-      (p/delay
-        100000
-        (fn [] (-> (swap! retry-countdown dec)
-                   (swap! request-tldr (get-url url))))))
-    request-tldr))
+    (loop [result request-tldr]
+      (devlog "tldr/summarize-loop" result url)
+      (cond
+        ;; continue the loop while status is 202
+        (= (:status result) 202)
+        (do
+          (devlog "tldr/summarize-url waiting..." url)
+          (-> (p/delay 2000)
+              (p/then #(get-url url))))
+
+        ;; status 200 - returns the result
+        (= (:status result) 200)
+        result
+
+        ;; status outside the 200 range - returns error
+        :else
+        {:body
+         (str "tldr.chat/error - status " (:status result) " / body: " (:body result))}))))
