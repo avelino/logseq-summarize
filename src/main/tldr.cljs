0ac4695424daf8392d509d924108eeb467977359
Avelino <31996+avelino@users.noreply.github.com>
2024-12-30 19:44:05 -0300

    tldr: create handle to work with requests
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/main/tldr.cljs
+++ src/main/tldr.cljs
@@ -2,50 +2,58 @@
   (:require [clojure.string :as string]
             [lambdaisland.fetch :as fetch]
             [promesa.core :as p]
+            [config :as cfg]
+            [errors :as e]
             [util :refer [devlog]]))
 
-(def ^:private api-host
-  "https://api.tldr.chat/v0")
+(def config (cfg/get-config))
 
 (defn format-md-output
-  "auto fix markdown output from tldr.chat"
+  "Pure function to format markdown output"
   [content]
-  ;; TODO: use a library that auto-fixed markdown
-  (string/replace content "- " "* "))
+  (string/replace content "- " (-> config :formatting :markdown-list-marker)))
 
-;; curl -H 'Accept: text/markdown' https://tldr.chat/url/https://xkcd.com/1438/
+(defn- build-request-url
+  "Pure function to build API request URL"
+  [endpoint url]
+  (str (-> config :api :host) endpoint url))
+
+(defn- handle-response
+  "Pure function to process API response"
+  [resp]
+  (let [body (:body resp)]
+    (if (e/api-error? resp)
+      (e/error->response (:status resp) body)
+      {:status (:status resp)
+       :body (format-md-output body)})))
+
 (defn- get-url
   "GET url from tldr.chat in format of markdown"
   [url]
-  (p/let [req-url (str api-host "/url/" url)
+  (p/let [req-url (build-request-url "/url/" url)
           resp (fetch/get req-url {:headers {:Accept "text/markdown"}})
-          body (:body resp)]
-    (devlog "tldr/get-url" req-url (format-md-output body))
-    (assoc resp :body (format-md-output body))))
+          result (handle-response resp)]
+    (devlog "tldr/get-url" req-url result)
+    result))
 
-;; curl -X POST --data url=https://xkcd.com/1438/ https://tldr.chat/summarize
 (defn summarize-url
   "POST url to tldr.chat and get summary in format of markdown"
   [url]
-  (p/let [_ (fetch/post (str api-host "/summarize")
+  (p/let [_ (fetch/post (build-request-url "/summarize" "")
                         {:headers {:Content-Type "application/json"}
-                         :body (fetch/encode-body :json {:url url})}) ;; first request to notify tldr.chat that we are waiting
+                         :body (fetch/encode-body :json {:url url})})
           request-tldr (get-url url)]
     (loop [result request-tldr]
-      (devlog "tldr/summarize-loop" result url)
       (cond
-        ;; continue the loop while status is 202
         (= (:status result) 202)
         (do
           (devlog "tldr/summarize-url waiting..." url)
-          (-> (p/delay 2000)
+          (-> (p/delay (-> config :api :timeout))
               (p/then #(get-url url))))
-
-        ;; status 200 - returns the result
+              
         (= (:status result) 200)
         result
-
-        ;; status outside the 200 range - returns error
+        
         :else
-        {:body
-         (str "tldr.chat/error - status " (:status result) " / body: " (:body result))}))))
+        (e/error->response (:status result) 
+                          (str "tldr.chat/error - " (:body result)))))))
