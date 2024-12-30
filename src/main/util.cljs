84e1a3de223959fefc6537644806cdb3e03bde74
Avelino <31996+avelino@users.noreply.github.com>
2024-12-30 19:42:33 -0300

    util: use spec
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/main/util.cljs
+++ src/main/util.cljs
@@ -1,67 +1,97 @@
 (ns util
   "Utility helpers for data transformation, DOM etc..."
-  (:require [clojure.walk :refer [keywordize-keys]]
+  (:require [clojure.spec.alpha :as s]
+            [clojure.walk :refer [keywordize-keys]]
             [cuerdas.core :as str]
             [goog.html.textExtractor :as gtext]
             [medley.core :refer [filter-vals]]))
 
-;; https://github.com/google/closure-library/blob/master/closure/goog/html/textextractor.js#L13
+;; Specs
+(s/def ::url string?)
+(s/def ::label string?)
+(s/def ::link string?)
+(s/def ::md-link (s/keys :req-un [::label ::link]))
+(s/def ::mime-type string?)
+(s/def ::charset string?)
+(s/def ::content-type (s/keys :req-un [::mime-type] :opt-un [::charset]))
+
+;; Debug helpers
 (def decode-html-content gtext/extractTextContent)
 
-(defn devlog [& msgs]
+(defn devlog 
+  "Pure logging function that only runs in debug mode"
+  [& msgs]
   (when goog.DEBUG
     (apply js/console.log (into ["Logseq Summarize"] msgs))))
 
-(defn target-value [e]
+;; DOM helpers
+(defn target-value 
+  "Pure function to extract value from DOM event"
+  [e]
   (.. e -target -value))
 
-(defn target-checked [e]
+(defn target-checked 
+  "Pure function to extract checked state from DOM event"
+  [e]
   (.. e -target -checked))
 
-(defn ednize [data]
+;; Data transformation
+(defn ednize 
+  "Pure function to convert JS data to EDN"
+  [data]
   (js->clj data :keywordize-keys true))
 
-;; https://github.com/lambdaisland/uri maybe useful
-(defn url? [s]
+;; URL handling
+(defn url? 
+  "Pure predicate to check if string is valid URL"
+  [s]
+  {:pre [(string? s)]}
   (try
     (do (js/URL. s) true)
-    (catch js/Object e false)))
+    (catch js/Object _ false)))
 
-(defn http? [s]
+(defn http? 
+  "Pure predicate to check if string is HTTP URL"
+  [s]
+  {:pre [(string? s)]}
   (and (str/starts-with? s "http")
        (url? s)))
 
+;; Markdown link handling
 (defn str->md-link
-  "Parse a string and return a map of :label & :link."
+  "Pure function to parse markdown link string into map"
   [s]
+  {:pre [(string? s)]}
   (some->> (str/trim s)
            (re-find #"\[(.*?)\]\((.*?)\)")
            rest
            (#(-> {:label (first %) :link (second %)}))))
 
-(defn md-link->str [{:keys [label link]}]
+(defn md-link->str 
+  "Pure function to convert markdown link map to string"
+  [{:keys [label link] :as md-link}]
+  {:pre [(s/valid? ::md-link md-link)]}
   (str/format "[%s](%s)" label link))
 
-(defn md-link? [s]
+(defn md-link? 
+  "Pure predicate to check if string is markdown link"
+  [s]
+  {:pre [(string? s)]}
   (some? (:link (str->md-link s))))
 
 (defn extract-link
-  "extract link from current line content"
+  "Pure function to extract link from content"
   [line-content]
-  ;; TODO: extract all links from a string and return link list
+  {:pre [(string? line-content)]}
   (if (md-link? line-content)
     (:link (str->md-link line-content))
     line-content))
 
-(comment
-  (str->md-link "[I'm label](I'm link)")
-  (md-link->str {:label "I'm label" :link "Just a link"}))
-
-(defn content-type
-  "Return a map of :mime-type and :charset etc...
-   from http Content-Type string say: 'application/json; charset=utf-8'
-   TODO: Better use proper http client lib"
+;; Content type handling
+(defn parse-content-type
+  "Pure function to parse content type string"
   [s]
+  {:pre [(string? s)]}
   (some->> (str/trim s)
            (re-find #"(.*?);\s*(.*?)$")
            rest
@@ -70,48 +100,37 @@
            keywordize-keys))
 
 (defn json-response?
+  "Pure predicate to check if content type is JSON"
   [content-type-str]
-  (= "application/json" (:mime-type (content-type content-type-str))))
+  {:pre [(string? content-type-str)]}
+  (= "application/json" (:mime-type (parse-content-type content-type-str))))
 
-(comment
-  (content-type "application/json; charset=utf-8")
-  (json-response? "application/json; charset=utf-8"))
-
-(defn nested? [data]
-  (cond
-    (not (or (map? data) (sequential? data))) false
-    (not (seq data)) false
-    (or (map? data) (sequential? data)) true
-    :else false))
+;; Data structure helpers
+(defn nested?
+  "Pure predicate to check if data structure is nested"
+  [data]
+  (and (or (map? data) (sequential? data))
+       (seq data)))
 
 (defn attrs-and-children
-  "Split flat values and nested values of a map"
+  "Pure function to split map into flat and nested values"
   [data]
-  [(filter-vals #(not (nested? %)) data)
-   (filter-vals #(nested? %) data)])
-
-; https://stackoverflow.com/questions/15020669/clojure-multiline-regular-expression
-(defn else-and-last [s]
-  (->> (str/rtrim s)
-       (re-find #"(?is)(.*?\s*)(\[.*?\]\(.*?\)|\S+?)$")
-       rest))
-
-(defn to-fixed [number places]
-  (.toFixed number places))
-
-(defn records? [data]
-  (and (sequential? data), (map? (first data))))
+  {:pre [(map? data)]}
+  [(filter-vals (complement nested?) data)
+   (filter-vals nested? data)])
 
 (defn tokenize-str
-  "Parse a string of words into a vector of tokens by both comma and whitepace."
+  "Pure function to split string into tokens"
   [s]
+  {:pre [(string? s)]}
   (-> (str/trim s)
       (str/replace #"," " ")
       (str/split #"\s+")))
 
 (defn exclude-include-ks
-  "Exclude then include keys from a map."
+  "Pure function to filter map keys"
   [m exclude-keys include-keys]
+  {:pre [(map? m) (sequential? exclude-keys) (sequential? include-keys)]}
   (cond-> m
     (seq exclude-keys) (select-keys (remove (set exclude-keys) (keys m)))
     (seq include-keys) (select-keys include-keys)))
